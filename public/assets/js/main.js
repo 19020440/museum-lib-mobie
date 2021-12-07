c57cd53b3c0910679d9f205913f821943aab052d
truongnam1 <namtao100@gmail.com>
2021-12-07 23:03:27 +0700

    thêm scan

---
+++ public/assets/js/main.js
@@ -0,0 +1,7 @@
+function on() {
+    $(".overlay-main").css('display', 'block');
+}
+
+function off() {
+    $(".overlay-main").css('display', 'none');
+}
--- public/assets/js/scan.js
+++ public/assets/js/scan.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#modalSelect .btn-submit').click(submitCamera);
-    $('#opencamera').click(openCamera);
-
+    // $('#opencamera').click(openCamera);
+    openCamera();
 
 })
 
@@ -10,12 +10,15 @@ function openCamera() {
     Html5Qrcode.getCameras().then(devices => {
         console.log(devices);
         if (devices && devices.length) {
+            changeCamera(devices);
+            console.log('ok camera');
             var html = '';
             for (const device of devices) {
 
                 html += `<option value="${device.id}">${device.label}</option>`
             }
             $('#select-camera').html(html);
+            $("#modalSelect").modal('show')
         }
     }).catch(err => {
         // handle err
@@ -26,25 +29,39 @@ function submitCamera() {
     var idCamera = $('#select-camera').val();
     console.log(`resCamera`, idCamera);
     if (idCamera) {
+
         var cameraId = idCamera;
         // .. use this to start scanning.
         const html5QrCode = new Html5Qrcode( /* element id */ "reader");
         html5QrCode.start(
                 cameraId, {
-                    fps: 1, // Optional, frame per seconds for qr code scanning
+                    fps: 10, // Optional, frame per seconds for qr code scanning
                     qrbox: 250
                 },
                 (decodedText, decodedResult) => {
                     // do something when code is read
                     console.log(`decodedText`, decodedText);
                     console.log(`decodedResult`, decodedResult);
+                    if (decodedText === '123456') {
+                        toastr.success('Đáp án chính xác');
+                        setTimeout(function() {
+                            // window.location.href = './done.html'     
+                        }, 2000);
+                    }
 
                 },
                 (errorMessage) => {
                     // parse error, ignore it.
-                })
+                }).then(function(r) {
+                $('.control-camera').css('display', 'flex');
+            })
             .catch((err) => {
                 // Start failed, handle it.
             });
     }
+}
+
+function changeCamera(data) {
+    console.log('change');
+    $('#modalSelect .res').html(JSON.stringify(data));
 }
